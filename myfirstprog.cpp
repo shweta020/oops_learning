0605947bcec59784a41199b87885c32122876532
Shubham Kumar <113201438+shubham-nitr@users.noreply.github.com>
2022-12-22 12:17:42 +0000

    Pls

--- myfirstprog.cpp
+++ myfirstprog.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "MY NAME IS SHWETA SINGH" << endl ;
-    cout << "THIS MY FIRST PROGRAME" << endl ;
+    cout << "MY NAME IS SHWETA SINGH" << endl ;//Hey pls don't leave me.
+    cout << "THIS MY FIRST PROGRAME" << endl ;//Shweta, don't go.
     return 0;
 }
